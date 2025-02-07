56d4242da5ffda4ae0e00d486eee994b1d89899a
JoGoRef <johannes.refsnes@gmail.com>
2025-02-07 11:00:14 +0100

    ulike funksjoner i js

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ const board = document.getElementById("gameboard");
 // definere spill variabler
 let slange = [{x: 10, y:10}]
 
+// tegner spillet, slangen og maten
+function tegn() {
+    board.innerHTML = "";
+    tegnslange();
+}
+
+// tegn slange 
+
+function tegnslange() { 
+    slange.forEach((segment) => {
+        const snakeElement = createGameElement("div", "snake");
+        setPosition(snakeElement, segment)
+        board.appendChild(snakeElement)
+    })
+}
+
+// lage en slange eller mat div
+function createGameElement(tag, className) {
+    const element = document.createElement(tag); 
+    element.className = className;
+    return element; 
+}
+
+// setter en posisjon til slangen eller maten 
+function setPosition(element, position) {
+    element.style.gridColumn = position.x; //betyr altså x = 10
+    element.style.gridRow = position.y; // betyr altså y = 10
+}
+    
+// tester tegne funksjon
+tegn()
+
